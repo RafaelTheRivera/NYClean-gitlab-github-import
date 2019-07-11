d4bdf447d3891561a8efd00a4e8ce694a5808b8a
Paige <paigemin@Paiges-MacBook-Pro.local>
2019-07-11 13:37:07 -0400

    add gitignore

---
+++ nyclean/src/Home/Home.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends Component {
+  constructor(){
+    super();
+    this.state = {};
+  }
+  render(){
+    return(
+      <div>
+        <header>
+        NYCLEAN
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Home;
