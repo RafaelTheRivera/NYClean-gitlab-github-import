9ae599971736b1a3837e87898956288ed56f906e
rarivera <ralphy.triguy@gmail.com>
2019-07-11 15:29:56 -0400

    final changes for thursday

--- nyclean/src/Universal/header.js
+++ nyclean/src/Universal/header.js
@@ -10,7 +10,7 @@ class Header extends Component {
     this.setState(this.search);
   }
   checkPageID(){
-    
+
   }
   checkLoginStatus(){
 
@@ -24,9 +24,9 @@ class Header extends Component {
           <img alt="" src="https://images.vexels.com/media/users/3/143356/isolated/preview/64e14fe0195557e3f18ea3becba3169b-search-magnifying-glass-by-vexels.png" id="magnifying"/></button>
           </div> </form>
           <div class = "headerItem" id = "logo"><a href="./">NYCLEAN</a></div>
-          <div class = "headerItem" id = "login">Log in<div id="profile">
+          <a href = "./login"><div class = "headerItem" id = "login">Log in<div id="profile">
           <img alt="" id = "profilepic" src = "https://m.media-amazon.com/images/M/MV5BMTc0MDMyMzI2OF5BMl5BanBnXkFtZTcwMzM2OTk1MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg"/>
-          </div></div>
+          </div></div></a>
         </div>
       </div>
     )
