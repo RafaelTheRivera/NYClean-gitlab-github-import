71205ddf2d4a837d09e7a8d8520d2a41bbef1ca3
rarivera <ralphy.triguy@gmail.com>
2019-07-11 14:22:50 -0400

    add header component

---
+++ nyclean/src/Universal/header.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import './../App.css';
+
+class Header extends Component {
+  constructor(){
+    super();
+    this.state = {search:0};
+  }
+  updateSearchBar(){
+    this.setState(this.search);
+  }
+  render(){
+    return(
+      <div>
+        <div id="bigHeader">
+          <form> <div class = "headerItem" id = "search">
+          <input type = "text" id="box"></input><button type = "submit" id="submit" class = "headerItem">
+          <img src="https://images.vexels.com/media/users/3/143356/isolated/preview/64e14fe0195557e3f18ea3becba3169b-search-magnifying-glass-by-vexels.png" id="magnifying"/></button>
+          </div> </form>
+          <div class = "headerItem" id = "logo">NYCLEAN</div>
+          <div class = "headerItem" id = "login"><div id="profile"><img src="{userProfile}"/></div>Log in</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header;
---
+++ nyclean/src/routes.js
@@ -0,0 +1,13 @@
+import App from './App';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+const Routes = () => (
+  <Router>
+    <div>
+      <Route exact path={"/"} component={() => <App />}/>
+    </div>
+  </Router>
+)
+
+export default Routes;
