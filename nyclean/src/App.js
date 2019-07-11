5d4b8d952c2184cb03d345cb4b4cc876aab08386
Roooooster <ian.xiong2002@gmail.com>
2019-07-11 13:12:41 -0400

    Add existing file

--- nyclean/src/App.js
+++ nyclean/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import AppComponent from './AppComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {};
+  }
+  render(){
+    return(
+      <div>
+        <p>This is what the very first page of my app looks like.</p>
+        <AppComponent />
+      </div>
+
+    );
+  }
 }
 
 export default App;
