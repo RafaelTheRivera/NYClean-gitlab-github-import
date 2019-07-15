da4075a47169d67c8c7c66b638d04854d36e1830
Paige <paigemin@Paiges-MacBook-Pro.local>
2019-07-15 09:42:45 -0400

    rafael

--- nyclean/src/routes.js
+++ nyclean/src/routes.js
@@ -1,4 +1,3 @@
-import Home from "./Home/Home";
 import Safety from "./Safety/Safety";
 import Profile from './Profile';
 import App from './App';
@@ -8,10 +7,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 const Routes = () => (
   <Router>
     <div>
-      <Route exact path = {"/"} component = {() => <Home />}/>
       <Route exact path = {"/safety"} component = {() => <Safety />}/>
       <Route exact path = {"/profPage"} component = {() => <Profile />}/>
-      <Route exact path = {"/app"} component = {() => <App />}/>
+      <Route exact path = {""} component = {() => <App />}/>
     </div>
   </Router>
 );
