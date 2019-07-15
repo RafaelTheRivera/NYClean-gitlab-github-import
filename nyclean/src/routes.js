0d3d1063eef3acbbd170d82d41c0484ef77c188c
rarivera <ralphy.triguy@gmail.com>
2019-07-15 13:31:40 -0400

    fixing header bugs

--- nyclean/src/routes.js
+++ nyclean/src/routes.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import Home from "./Home/Home";
-import Safety from "./Safety/Safety";
-import Profile from './Profile';
-<<<<<<< HEAD
-import Home from './Home';
-import Edit from './Edit';
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-
-const Routes = () => (
-    <Router>
-      <div>
-        <Route exact path = {"/"} component = {() => <Home />}/>
-        <Route exact path = {"/profPage"} component = {() => <Profile />}/>
-        <Route exact path = {"/Edit"} component = {() => <Edit />}/>
-      </div>
-    </Router>
-=======
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-const Routes = () => (
-  <Router>
-    <div>
-      <Route exact path = {"/"} component = {() => <Home />}/>
-      <Route exact path = {"/safety"} component = {() => <Safety />}/>
-      <Route exact path = {"/profPage"} component = {() => <Profile />}/>
-    </div>
-  </Router>
->>>>>>> 05ae0e96bd5d9eaf56aebc863ee0acfd175cccfd
-=======
 import Safety from "./Safety/Safety";
 import Profile from './Profile';
 import App from './App';
@@ -46,7 +14,6 @@ const Routes = () => (
       <Route exact path = {"/"} component = {() => <App />}/>
     </div>
   </Router>
->>>>>>> debc2f45e6f0b67b63149476b44164bd96e01a06
 );
 
 export default Routes;
