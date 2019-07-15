79d67a0c51d720a270b73c1486bc8f775d423ba5
rarivera <ralphy.triguy@gmail.com>
2019-07-15 13:20:07 -0400

    updated header

--- nyclean/src/Universal/header.js
+++ nyclean/src/Universal/header.js
@@ -6,21 +6,21 @@ class Header extends Component {
     super();
     this.state = {search:0};
   }
-  updateSearchBar(){
+  updateSearchBar = e => {
     this.setState(this.search);
     //find variable to search in search bar
   }
-  checkPageID(){
+  checkPageID = e => {
     //if not at home page, cover search bar
     //note: if someone wants to, you can skip this step by copying this header object and recreating it without a search bar, then specifically calling it on other pages.
   }
-  checkLoginStatus(){
+  checkLoginStatus = e => {
     //redirect to log in page if not logged in.
   }
-  getProfilePicture(){
+  getProfilePicture = e => {
     //find user's profile photo
   }
-  getSearch(){
+  getSearch = e => {
     //find search results based on updateSearchBar
     //note: must call the search results bar
   }
@@ -28,14 +28,31 @@ class Header extends Component {
     return(
       <div>
         <div id="bigHeader">
-          <form> <div class = "headerItem" id = "search">
-          <input type = "text" id="box"></input><button type = "submit" id="submit" class = "headerItem">
-          <img alt="" src="https://images.vexels.com/media/users/3/143356/isolated/preview/64e14fe0195557e3f18ea3becba3169b-search-magnifying-glass-by-vexels.png" id="magnifying"/></button>
-          </div> </form>
-          <div class = "headerItem" id = "logo"><a href="./">NYCLEAN</a></div>
-          <a href = "./login"><div /* this is actually useless*/class = "headerItem" id = "login"> /*"Log in" should actually just reflect the username*/Log in<div id="profile">
-          <img alt="" id = "profilepic" src = /*should actually link to individual profiles*/"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
-          </div></div></a>
+          <form>
+            <div class = "headerItem" id = "search">
+              <input
+                type = "text"
+                id="box"
+                name = "search"
+                placeholder = " Search..."
+                onChange = {this.updateSearchBar}
+                /*value = {this.state.search}*/></input>
+              <button type = "submit" id="submit" class = "headerItem">
+                <img alt="" src="https://images.vexels.com/media/users/3/143356/isolated/preview/64e14fe0195557e3f18ea3becba3169b-search-magnifying-glass-by-vexels.png" id="magnifying"/>
+              </button>
+            </div>
+          </form>
+          <div class = "headerItem" id = "logo">
+            <a href="./">NYCLEAN</a>
+          </div>
+          <a href = "./login">
+            <div class = "headerItem" id = "login">
+              <span id="rogueText">My Profile</span>
+              <div id="profile">
+                <img alt="" id = "profilepic" src = /*should actually link to individual profiles*/"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     )
--- nyclean/src/routes.js
+++ nyclean/src/routes.js
@@ -2,14 +2,16 @@ import Safety from "./Safety/Safety";
 import Profile from './Profile';
 import App from './App';
 import React from "react";
+import User from './User';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const Routes = () => (
   <Router>
     <div>
+      <Route exact path = {"/user"} component = {() => <User />}/>
       <Route exact path = {"/safety"} component = {() => <Safety />}/>
       <Route exact path = {"/profPage"} component = {() => <Profile />}/>
-      <Route exact path = {""} component = {() => <App />}/>
+      <Route exact path = {"/"} component = {() => <App />}/>
     </div>
   </Router>
 );
