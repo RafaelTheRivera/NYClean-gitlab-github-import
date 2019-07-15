68e86db93d52499a034b4817fc5997496baed46f
rtdrtd67 <guethan@yahoo.com>
2019-07-15 13:20:40 -0400

    ethan3

--- nyclean/src/Universal/header.js
+++ nyclean/src/Universal/header.js
@@ -25,7 +25,7 @@ class Header extends Component {
     //note: must call the search results bar
   }
   checkPageID(){
-    
+
   }
   checkLoginStatus(){
 
@@ -36,30 +36,22 @@ class Header extends Component {
         <div id="bigHeader">
           <form> <div class = "headerItem" id = "search">
           <input type = "text" id="box"></input><button type = "submit" id="submit" class = "headerItem">
-<<<<<<< HEAD
-<<<<<<< HEAD
           <img src="https://images.vexels.com/media/users/3/143356/isolated/preview/64e14fe0195557e3f18ea3becba3169b-search-magnifying-glass-by-vexels.png" id="magnifying" alt = "img"/></button>
           </div> </form>
           <div class = "headerItem" id = "logo">NYCLEAN</div>
           <div class = "headerItem" id = "login"><div id="profile"><img src="{userProfile}" alt = "user"/></div>Log in</div>
-=======
           <img alt="" src="https://images.vexels.com/media/users/3/143356/isolated/preview/64e14fe0195557e3f18ea3becba3169b-search-magnifying-glass-by-vexels.png" id="magnifying"/></button>
           </div> </form>
           <div class = "headerItem" id = "logo"><a href="./">NYCLEAN</a></div>
           <div class = "headerItem" id = "login">Log in<div id="profile">
           <img alt="" id = "profilepic" src = "https://m.media-amazon.com/images/M/MV5BMTc0MDMyMzI2OF5BMl5BanBnXkFtZTcwMzM2OTk1MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg"/>
           </div></div>
->>>>>>> 05ae0e96bd5d9eaf56aebc863ee0acfd175cccfd
-=======
           <img alt="" src="https://images.vexels.com/media/users/3/143356/isolated/preview/64e14fe0195557e3f18ea3becba3169b-search-magnifying-glass-by-vexels.png" id="magnifying"/></button>
-          </div> </form>
+          </div> </form></div>
           <div class = "headerItem" id = "logo"><a href="./">NYCLEAN</a></div>
           <a href = "./login"><div /* this is actually useless*/class = "headerItem" id = "login"> /*"Log in" should actually just reflect the username*/Log in<div id="profile">
           <img alt="" id = "profilepic" src = /*should actually link to individual profiles*/"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
           </div></div></a>
->>>>>>> debc2f45e6f0b67b63149476b44164bd96e01a06
-        </div>
-      </div>
     )
   }
 }
