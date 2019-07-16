ef1aac8caf99e3327a6e9877fc58aebd9d3eb78e
Paige <paigemin@Paiges-MacBook-Pro.local>
2019-07-16 14:44:53 -0400

    some profile pg formatting

--- nyclean/src/Profile.js
+++ nyclean/src/Profile.js
@@ -11,18 +11,33 @@ class Profile extends Component {
   return (
     <div class = "appText">
     <Header/>
+
+    <header>
+    <a href=".">BACK</a>
+    </header>
+
     <center><h1>Account Details</h1></center>
-    <h3>Profile Picture</h3>
-    <img src = "https://m.media-amazon.com/images/M/MV5BMTc0MDMyMzI2OF5BMl5BanBnXkFtZTcwMzM2OTk1MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg" alt = "Morgan"/>
-    <p><a href = "https://www.google.com/search?q=morgan+freeman&rlz=1C1CHBD_enUS854US854&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjqg-7lva3jAhXlc98KHVSODB0Q_AUIESgC&biw=1366&bih=625&safe=active&ssui=on">Edit Profile Picture</a></p>
-    <h3>Pins:</h3>
-    <ol>
-      <li>20 E 18th Street</li>
-      <li>58 W 72nd Street</li>
-      <li>103 E 8th Street</li>
-    </ol>
-    <p>Username: {this.state.User}  <a href = "/Edit">Edit Username</a></p>
-    <p>Total trash picked up(lbs): {this.state.Totaltrash}</p>
+
+    <div id="profilecircle">
+    <img id = "profileimg" src = "https://m.media-amazon.com/images/M/MV5BMTc0MDMyMzI2OF5BMl5BanBnXkFtZTcwMzM2OTk1MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg" alt = "Morgan"/>
+    </div>
+
+    <h2 id = "username">username</h2>
+
+    <p id = "bio">This is my bio. I'm really cool and I like to pick up trash.
+                  I have a dog named Dog and I like chocolate ice cream.</p>
+
+    <div id="profilepin">
+      <h3>Pins:</h3>
+      <ol>
+        <li>20 E 18th Street</li>
+        <li>58 W 72nd Street</li>
+        <li>103 E 8th Street</li>
+      </ol>
+
+    <p>Trash count: {this.state.Totaltrash} lbs</p>
+    </div>
+
     </div>
   );
   }
