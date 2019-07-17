3178a3840da7ebebedfa3a0ba1528feead59df75
rarivera <ralphy.triguy@gmail.com>
2019-07-17 11:11:22 -0400

    post-paige update error fixes

--- nyclean/src/Universal/header.js
+++ nyclean/src/Universal/header.js
@@ -79,14 +79,9 @@ class Header extends Component {
           <a href = ".">
             <div className= "headerItem" id = "login" style = {{width: this.state.profileWidth}}>
               <span id="rogueText">{this.state.username}</span>
-            <div className= "headerItem" id = "login">
-              <span id="rogueText">My Profile</span>
-              <div id="profile">
-                <img alt="" id = "profilepic" src = /*should actually link to individual profiles*/"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
-              </div>
+              <img alt="" id = "profilepic" src = /*should actually link to individual profiles*/"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"/>
             </div>
           </a>
-
       </div>
     )
   }
