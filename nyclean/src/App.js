25391a7c55e668b678521bd113b47f5662bd12de
Paige <paigemin@Paiges-MacBook-Pro.local>
2019-07-23 11:57:57 -0400

    styling~~

--- nyclean/src/App.js
+++ nyclean/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Header from './Universal/header.js';
 import './App.css';
 import footergradient from './images/footergradient.png';
+import safetyicon from './images/safetyicon.png';
 import Map from './Universal/map.js'
 import Bubble from './Universal/bubble.js';
 import { Redirect } from 'react-router-dom';
@@ -69,10 +70,11 @@ class App extends Component {
 
           <Map />
 
+          <a href="./safety"><img id = "safetyicon" src = {safetyicon} alt = "safetyicon"/></a>
+
           <footer>
             <div id = "safetytab"></div>
             <button onClick = {this.signOut}>Sign Out</button>
-            <a id = "safety" href="./safety">Safety Information</a>
           </footer>
 
 
--- nyclean/src/Universal/bubble.js
+++ nyclean/src/Universal/bubble.js
@@ -150,9 +150,23 @@ class Bubble extends Component{
             <div className = "blocker" id = "blo1">
             </div>
             <div className = "bubble" id = "bub1">
-              Post by: paige
-              <center><div id="insertimage"></div></center>
-              Insert caption here
+              <p className = "small">Post by: paige
+              <center><div id="insertimage">
+
+              <img src = {this.state.imageSrc}/>
+                    <form onSubmit = {this.submitInput}>
+                    <input
+                    type = "images"
+                    name = "profilePic"
+                    placeholder = "Image URL"
+                    onChange = {this.updateInput}
+                    value = {this.state.imageSrc}
+                    />
+                    <button type = "submit">Submit</button>
+                    </form>
+              </div></center>
+              Insert caption here</p>
+              <img id = "add" src = {add} alt = "add poster"/>
             </div>
             <img className = "cover" id = "cover1" src = {cover} alt = "cover"/>
           </span>
@@ -164,8 +178,8 @@ class Bubble extends Component{
             </div>
             <div className = "bubble" id = "bub2">
             </div>
-            <img id = "add" src = {add} alt = "addposter"/>
             <img className = "cover" id = "cover2" src = {cover} alt = "cover"/>
+
           </span>
 
         <img id = "leader" src = {leader} alt = {"leaderboard"} onClick = {this.openLeader}/>
@@ -175,15 +189,16 @@ class Bubble extends Component{
             <div className = "blocker" id = "blo3">
             </div>
             <div className = "bubble" id = "bub3">
-            <center><p id = "totalcount">TOTAL COUNT</p> <p id = "livecount">12,345 lbs</p> <br />
+            <center><p id = "totalcount">TOTAL COUNT</p> <p id = "livecount"><b>12,345</b> lbs</p> <br />
             Weekly Leaderboard</center>
-            <ol>
+            <br />
+            <p className = "small"><ol>
               <li>user 123 lbs</li>
               <li>user 123 lbs</li>
               <li>user 123 lbs</li>
               <li>user 123 lbs</li>
               <li>user 123 lbs</li>
-            </ol>
+            </ol></p>
             </div>
             <img className = "cover" id = "cover3" src = {cover} alt = "cover"/>
           </span>
